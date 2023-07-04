dfec3d66046554eabf2249f3bc270096b2cdc2f2
Nike073i <nike073i@mail.ru>
2023-07-04 22:39:02 +0400

    Урок 6. Стиль окна WPF

--- src/WPF/Exlorer.WPF.UI/MainWindow.xaml.cs
+++ src/WPF/Exlorer.WPF.UI/MainWindow.xaml.cs
@@ -1,13 +1,34 @@
 ﻿using Explorer.Shared.ViewModels;
+using System.Windows;
 
 namespace Exlorer.WPF.UI
 {
     public partial class MainWindow
     {
+        private readonly MainViewModel _mainViewModel;
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = new MainViewModel();
+            _mainViewModel = new MainViewModel();
+            DataContext = _mainViewModel;
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ExpandButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+                WindowState = WindowState.Normal;
+            else
+                WindowState = WindowState.Maximized;
+        }
+
+        private void CollapseButton_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
         }
     }
 }
