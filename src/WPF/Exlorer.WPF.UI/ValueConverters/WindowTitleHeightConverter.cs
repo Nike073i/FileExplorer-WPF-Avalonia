0e2392656f3d4098a5dde92a67c6b522774250a4
Nike073i <nike073i@mail.ru>
2023-07-06 21:55:53 +0400

    Оформление тайтл бара и перемещение окна  WPF

---
+++ src/WPF/Exlorer.WPF.UI/ValueConverters/WindowTitleHeightConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Exlorer.WPF.UI
+{
+    internal class WindowTitleHeightConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is WindowState windowState)
+            {
+                return windowState == WindowState.Normal ? 42 : 32;
+            }
+            return 42;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
